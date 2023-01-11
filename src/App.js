6075f564386c802f7248e224e26577f1997d7552
Pablo Dominguez <pasaxo@gmail.com>
2023-01-11 17:27:34 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Switch } from 'react-router-dom';
 import logo from './trivia.png';
+import Login from './pages/Login';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA  VEZ</p>
-      </header>
+      <Switch>
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <Login />
+        </header>
+      </Switch>
     </div>
   );
 }
---
+++ src/redux/reducers/login.jsx
@@ -0,0 +1,17 @@
+export const LOGIN = 'LOGIN';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case LOGIN:
+    return { ...state,
+      email: action.email };
+  default:
+    return state;
+  }
+};
+
+export default user;
