8bffb3824af6305bb37a73d297b06703dc2d65af
Nathalia Marana <nathalia_marana@hotmail.com>
2023-01-14 15:12:12 -0300

    requisito 4 ok

---
+++ src/tests/helpers/loginPage.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndRedux } from './renderWithRouterAndRedux';
+import Login from '../../pages/Login';
+import App from '../../App';
+import getCurrentValue from '../../services/apiTrivia';
+import Game from '../../pages/Game';
+
+const emailTest = 'tryber@teste.com';
+
+describe('Testando a página Login', () => {
+    it('Verifica link da página', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+
+        const { pathname } = history.location;
+        expect(pathname).toBe('/');
+    });
+
+    it('Verifica se o botão configuração renderiza para /settings', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+
+        const btnSettings = screen.getByRole('button', {
+            name: /configuração/i
+        });
+        expect(btnSettings).toBeInTheDocument();
+
+        userEvent.click(btnSettings);
+        const { pathname } = history.location;
+        expect(pathname).toBe("/settings");
+    });
+
+    it('Verifica se o botão Play está sendo renderizado na tela quando habilitado', () => {
+        renderWithRouterAndRedux(<Login />);
+
+        const btnPlay = screen.getByRole('button', {
+            name: /play/i
+        });
+        // expect(btnPlay).toBeInTheDocument();
+
+        const userName = screen.getByTestId('input-player-name');
+        const userEmail = screen.getByTestId('input-gravatar-email');
+
+        userEvent.type(userName, 'teste');
+        userEvent.type(userEmail, emailTest);
+
+        expect(userEmail).toHaveValue('tryber@teste.com');
+
+        expect(btnPlay).toBeEnabled();
+
+    });
+
+
+    it('Verifica o botão Play mudar para rota /game', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+
+        const userName = screen.getByTestId('input-player-name');
+        const userEmail = screen.getByTestId('input-gravatar-email');
+
+        userEvent.type(userName, 'teste');
+        userEvent.type(userEmail, 'tryber@teste.com');
+
+        const btnPlay = screen.getByRole('button', {
+            name: /play/i
+        });
+        userEvent.click(btnPlay);
+
+        const { pathname } = history.location;
+        setTimeout(() => { expect(pathname).toBe("/game"); }, 5000);
+    });
+
+    it('Verifica se a função getCurrentValue está trabalhando corretamente e retornando o token', async () => {
+        const response = await getCurrentValue();
+        expect(response).toHaveProperty('token');
+    });
+
+});
